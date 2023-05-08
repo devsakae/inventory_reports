6f5694864927ad486998ffee7b7e153b09b80d64
Rodrigo Sakae <devsakae@gmail.com>
2023-05-08 17:48:15 -0300

    feat: testes e simplereport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,28 @@
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(produtos):
+        df = []
+        dv = []
+        empresas = {}
+        for produto in produtos:
+            df.append(produto["data_de_fabricacao"])
+            if (
+                datetime.strptime(produto["data_de_validade"], "%Y-%m-%d")
+                >= datetime.today()
+            ):
+                dv.append(produto["data_de_validade"])
+            if produto["nome_da_empresa"] not in empresas:
+                empresas[produto["nome_da_empresa"]] = 0
+            empresas[produto["nome_da_empresa"]] += 1
+        dfma = min(df)
+        dvmp = min(dv)
+        ecmp = max(empresas, key=empresas.get)
+
+        return (
+            f"Data de fabricação mais antiga: {dfma}\n"
+            f"Data de validade mais próxima: {dvmp}\n"
+            f"Empresa com mais produtos: {ecmp}"
+        )
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,17 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    arroz = Product(
+        id=1,
+        nome_do_produto="Kiarroz",
+        nome_da_empresa="Fumacense",
+        data_de_fabricacao=2023,
+        data_de_validade=2024,
+        numero_de_serie="0010052023",
+        instrucoes_de_armazenamento="Guardar em local seco e arejado",
+    )
+
+    assert arroz.id == 1
+    assert arroz.nome_do_produto == "Kiarroz"
+    assert arroz.nome_da_empresa == "Fumacense"

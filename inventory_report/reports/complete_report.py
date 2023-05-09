abe2052f93e4dc9b37fb3419905007288737743b
Rodrigo Sakae <devsakae@gmail.com>
2023-05-09 15:31:51 -0300

    fix: quebra de linha após o retorno

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -6,8 +6,8 @@ class CompleteReport(SimpleReport):
     @staticmethod
     def generate(produtos):
         report = SimpleReport.generate(produtos)
-        report += "\nProdutos estocados por empresa:"
+        report += "\nProdutos estocados por empresa:\n"
         allproducts = Counter([p["nome_da_empresa"] for p in produtos])
         for name, qty in allproducts.items():
-            report += f"\n- {name}: {qty}"
+            report += f"- {name}: {qty}\n"
         return report

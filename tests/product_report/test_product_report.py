ed61e1575da1d6ec8543049e4b7d5bd0cfdb0a6c
Rodrigo Sakae <devsakae@gmail.com>
2023-05-10 16:44:07 -0300

    test de produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "Whisky",
+        "Dewars",
+        "2020-01-01",
+        "2030-01-01",
+        "00010120",
+        "em local seco e arejado",
+    )
+
+    response = produto.__repr__()
+    assert 'O produto Whisky fabricado em 2020-01-01' in response
+    assert 'por Dewars com validade até 2030-01-01' in response
+    assert 'precisa ser armazenado em local seco e arejado.' in response

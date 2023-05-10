96884ab2e33a28bb55090f1dd037357e97efedf2
Rodrigo Sakae <devsakae@gmail.com>
2023-05-10 09:44:48 -0300

    inventory made

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,2 +1,22 @@
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
 class Inventory:
-    pass
+    @staticmethod
+    def import_data(path, type):
+        if path.endswith('.csv'):
+            produtos = CsvImporter.import_data(path)
+        if path.endswith('.json'):
+            produtos = JsonImporter.import_data(path)
+        if path.endswith('.xml'):
+            produtos = XmlImporter.import_data(path)
+        if type == 'simples':
+            return SimpleReport.generate(produtos)
+        if type == 'completo':
+            return CompleteReport.generate(produtos)
+        else:
+            raise ValueError

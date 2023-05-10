c1d50467f3d967f0558c48461f31b1cecd184c2e
Rodrigo Sakae <devsakae@gmail.com>
2023-05-10 09:27:25 -0300

    fix: test product creation with all requirements tested

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -6,8 +6,8 @@ def test_cria_produto():
         id=1,
         nome_do_produto="Kiarroz",
         nome_da_empresa="Fumacense",
-        data_de_fabricacao=2023,
-        data_de_validade=2024,
+        data_de_fabricacao="2023-05-10",
+        data_de_validade="2024-06-29",
         numero_de_serie="0010052023",
         instrucoes_de_armazenamento="Guardar em local seco e arejado",
     )
@@ -15,3 +15,7 @@ def test_cria_produto():
     assert arroz.id == 1
     assert arroz.nome_do_produto == "Kiarroz"
     assert arroz.nome_da_empresa == "Fumacense"
+    assert arroz.data_de_fabricacao == "2023-05-10"
+    assert arroz.data_de_validade == "2024-06-29"
+    assert arroz.numero_de_serie == "0010052023"
+    assert arroz.instrucoes_de_armazenamento == "Guardar em local seco e arejado"
